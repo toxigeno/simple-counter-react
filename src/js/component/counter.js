72159904d428b7f751fa36f64a62db7e3686c346
toxigeno <melissaaraya@yahoo.com>
2021-03-04 03:18:21 +0000

    eliminado home

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 const Counter = props => {
@@ -28,27 +27,5 @@ Counter.propTypes = {
 	dg5: PropTypes.number,
 	dg6: PropTypes.number
 };
-
-let SecondsCounter = 0;
-setInterval(function() {
-	const digit1 = Math.floor(SecondsCounter);
-	const digit2 = Math.floor(SecondsCounter / 10);
-	const digit3 = Math.floor(SecondsCounter / 100);
-	const digit4 = Math.floor(SecondsCounter / 1000);
-	const digit5 = Math.floor(SecondsCounter / 10000);
-	const digit6 = Math.floor(SecondsCounter / 100000);
-	SecondsCounter++;
-	ReactDOM.render(
-		<Counter
-			dg1={digit1}
-			dg2={digit2}
-			dg3={digit3}
-			dg4={digit4}
-			dg5={digit5}
-			dg6={digit6}
-		/>,
-		document.querySelector("#app")
-	);
-}, 1000);
 
 export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,27 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import Counter from "./component/counter.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let SecondsCounter = 0;
+setInterval(function() {
+	const digit1 = Math.floor(SecondsCounter);
+	const digit2 = Math.floor(SecondsCounter / 10);
+	const digit3 = Math.floor(SecondsCounter / 100);
+	const digit4 = Math.floor(SecondsCounter / 1000);
+	const digit5 = Math.floor(SecondsCounter / 10000);
+	const digit6 = Math.floor(SecondsCounter / 100000);
+	SecondsCounter++;
+	ReactDOM.render(
+		<Counter
+			dg1={digit1}
+			dg2={digit2}
+			dg3={digit3}
+			dg4={digit4}
+			dg5={digit5}
+			dg6={digit6}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
